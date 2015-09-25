9a5ee4c66f8633369167eccff27991d7e3a9f2d0
Russell Nibbelink <russell.n@berkeley.edu>
2015-09-25 13:59:08 -0700

    hw2 complete

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,10 @@
 class Foobar
-  # ENTER CODE FOR Q2 HERE
+  def initialize(word)
+  	@word = word
+  end
+
+  def bar(s1, s2) 
+    puts 'Ruff! Ruff!' 
+    return "#{s1}"+@word+"#{s2[:sat]}"
+  end 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,30 @@
 class PagesController < ApplicationController
   def home
-    # foo = Foobar.new "baz"
-    # @baz = foo.bar :cat, sat: :dat, dat: :sat
+    foo = Foobar.new "baz"
+    @baz = foo.bar :cat, sat: :dat, dat: :sat
   end
 
   def stringify
-    @text = "You are nothing!"
+    if !params[:name].blank? && !params[:adjective].blank?
+      @text = params[:name] + " is so " + params[:adjective]
+    else
+      @text = "You are nothing!"
+    end
   end
 
   def age
   end
 
   def person
+    if !params[:name].blank? && !params[:age].blank?
+      person = Person.new params[:name], params[:age]
+      @introduction = person.introduce
+      @birth_year = person.birth_year
+      @nickname = person.nickname
+    else
+      @introduction = "Incomplete Form"
+      @birth_year = ""
+      @nickname = ""
+    end
   end
 end
